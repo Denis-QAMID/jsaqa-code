e81a448660f24cb257283409d73673f98ec17070
Denis Denisov <xeddyx@mail.ru>
2023-07-05 15:27:00 +0300

    Update App.spec

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -16,9 +16,10 @@ test("not a valid test", async ({ page }) => {
   await page.goto("https://netology.ru/?modal=sign_in");
   await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
   await page.locator('[placeholder="Email"]').click();
-  await page.locator('[placeholder="Email"]').fill("drugoimail@mail.ru");
+  await page.locator('[placeholder="Email"]').fill("vasyapypkin@mail.ru");
   await page.locator('[placeholder="Пароль"]').click();
-  await page.locator('[placeholder="Пароль"]').fill("4444");
+  await page.locator('[placeholder="Пароль"]').fill("123456");
   await page.locator('[data-testid="login-submit-btn"]').click();
-  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  const error = await page.locator('[data-testid="login-error-hint"]');
+  await expect(error).toHaveText("Вы ввели неправильно логин или пароль");
 });
